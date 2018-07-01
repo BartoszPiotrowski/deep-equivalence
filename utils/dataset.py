01f027da35de27172823603089b09e710eaff2eb
bartosz <bartoszpiotrowski@post.pl>
2018-07-01 08:43:00 +0200

    Init OK.

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 
 class Dataset:
@@ -11,7 +12,7 @@ class Dataset:
         self.labels = []
         with open(formulae_filename, 'r') as formulae:
             for line in formulae:
-                f1, f2, l = line.strip('\n').split(' ')
+                l, f1, f2 = line.strip('\n').split(' ')
                 self.formulae_1.append(f1)
                 self.formulae_2.append(f2)
                 self.labels.append(l)
@@ -23,11 +24,11 @@ class Dataset:
         self.formulae_lengths_1 = [len(f) for f in self.formulae_1]
         self.formulae_lengths_2 = [len(f) for f in self.formulae_2]
         self.shuffle_batches = shuffle_batches
-        self._permutation = np.random.permutation(len(self.sequences)) \
-                if self.shuffle_batches else np.arange(len(self.sequences))
+        self._permutation = np.random.permutation(len(self)) \
+                if self.shuffle_batches else np.arange(len(self))
 
-    def all_data(self):
-        return self.sequences, self.labels
+    def __len__(self):
+        return len(self.formulae_1)
 
     def padding(sequences, length, pad=0):
         padded_sequences = []
@@ -42,9 +43,10 @@ class Dataset:
             self._permutation[:batch_size], self._permutation[batch_size:]
         max_length = max(self.formulae_lengths_1[batch_perm] +
                          self.formulae_lengths_2[batch_perm])
-        sequences = padding(self.sequences[batch_perm], max_length)
+        seqs_1 = np.array(padding(self.seqs_1[batch_perm], max_length))
+        seqs_2 = np.array(padding(self.seqs_2[batch_perm], max_length))
         labels = self.labels[batch_perm]
-        return sequences, labels
+        return seqs_1, seqs_2, labels
 
     def epoch_finished(self):
         if len(self._permutation) == 0:
