effedc2de143b73a0748525999d79d70bf1ad3fd
bartosz <bartoszpiotrowski@post.pl>
2018-08-03 17:12:39 +0200

    Visualization for dim=2.

--- utils/visualize_embeddings_dim2.R
+++ utils/visualize_embeddings_dim2.R
@@ -1,4 +1,3 @@
-library(tsne)
 library(ggplot2)
 
 VOCAB_FILE<-'data/vocab.txt'
@@ -13,11 +12,10 @@ embeds_files <- Sys.glob(paste(path, EMBED_FILE_PATTERN, sep='/'))
 
 for (embeds_file in embeds_files){
 	embeds <- read.csv(embeds_file, header=F)
-	projection <- data.frame(tsne(embeds))
-	colnames(projection) <- c('v1', 'v2')
-	projection['symbol'] <- symbols
+	colnames(embeds) <- c('v1', 'v2')
+	embeds['symbol'] <- symbols
 	# TODO fixed grid
-	ggplot(projection, aes(x=v1, y=v2, label=symbol)) +
+	ggplot(embeds, aes(x=v1, y=v2, label=symbol)) +
 	       	geom_text(aes(label=symbol))
 	ggsave(sub('.csv', '.png', embeds_file))
 }
