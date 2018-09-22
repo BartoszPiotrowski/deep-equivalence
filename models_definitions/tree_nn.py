8f09cd84074e0959bad53a85c0d60537be99cd10
bartosz <bartoszpiotrowski@post.pl>
2018-09-22 18:18:33 +0200

    Not tested but looks not bad.

--- models_definitions/tree_nn.py
+++ models_definitions/tree_nn.py
@@ -1,18 +1,21 @@
-import random
 import torch
+import random
+import argparse
 import sys
 sys.path.append('.')
 from utils.parse import parse
+from utils.tools import one_hot
+from utils.dataset import DatasetTreeNN
 
 
-class PredicteNetwork(torch.nn.Module):
-    def __init__(self, predicate_name, predicate_arity,
-                 dim_in_out=64, dim_h=64):
-        super(PredicteNetwork, self).__init__()
-        self.predicate_name = predicate_name
-        self.predicate_arity = predicate_arity
+class FunctionNetwork(torch.nn.Module):
+    def __init__(self, function_name, function_arity,
+                 dim_in_out=32, dim_h=32):
+        super(FunctionNetwork, self).__init__()
+        self.function_name = function_name
+        self.function_arity = function_arity
         self.model = torch.nn.Sequential(
-            torch.nn.Linear(predicate_arity * dim_in_out, dim_h),
+            torch.nn.Linear(function_arity * dim_in_out, dim_h),
             torch.nn.ReLU(),
             torch.nn.Linear(dim_h, dim_in_out),
         )
@@ -21,23 +24,9 @@ class PredicteNetwork(torch.nn.Module):
         return self.model(x)
 
 
-class ConstantNetwork(torch.nn.Module):
-    # TODO bias=True?
-    def __init__(self, predicate_name, predicate_arity, dim_out=64):
-        super(ConstantNetwork, self).__init__()
-        self.predicate_name = predicate_name
-        self.predicate_arity = predicate_arity
-        self.model = torch.nn.Sequential(
-            torch.nn.Linear(1, dim_out),
-        )
-
-    def forward(self): # TODO can we ommit the second argument?
-        return self.model()
-
-
-class VariableNetwork(torch.nn.Module):
-    def __init__(self, dim_in=5, dim_h=64, dim_out=64):
-        super(VariableNetwork, self).__init__()
+class VarConstNetwork(torch.nn.Module):
+    def __init__(self, dim_in=8, dim_h=32, dim_out=32):
+        super(VarConstNetwork, self).__init__()
         self.model = torch.nn.Sequential(
             torch.nn.Linear(dim_in, dim_h),
             torch.nn.ReLU(),
@@ -49,168 +38,160 @@ class VariableNetwork(torch.nn.Module):
 
 
 class EqualityNetwork(torch.nn.Module):
-    def __init__(self, dim_in=64, dim_h=64):
+    def __init__(self, dim_in=32, dim_h=32):
         super(EqualityNetwork, self).__init__()
         self.model = torch.nn.Sequential(
             torch.nn.Linear(2 * dim_in, dim_h),
             torch.nn.ReLU(),
             torch.nn.Linear(dim_h, 1),
+            # TODO add sigmoid
         )
 
-    def forward(self, x):
+    def forward(self, x): # TODO this repeats; make a super-class
         return self.model(x)
 
 
-def loss(y_pred, y):
-    return (y_pred - y).pow(2).item() # TODO not pow(2) but |.|
+class TreeNN:
+    def __init__(
+        self,
+        functs_with_arits,
+        vars_consts_as_tensors,
+        loss=torch.nn.CrossEntropyLoss(),
+        optimizer=torch.optim.SGD(params_of_modules, lr=0.001, momentum=0.8)):
+        # TODO add parameter for controlling layer size
+
+        self.functs_with_arits = functs_with_arits
+        self.vars_consts_as_tensors = vars_consts_as_tensors
+        self.n_vars_consts = len(vars_consts_as_tensors)
+
+        # instantiate modules for all function symbols
+        self.modules = {}
+        # TODO add parameter for controlling layer size
+        for func in self.functs_with_arits:
+            modules[func] =  FunctionNetwork(func, self.functs_with_arits[func])
+        modules['VARCONST'] = VarConstNetwork(dim_in=self.n_vars_consts)
+        modules['EQUALITY'] = EqualityNetwork()
+        self.loss = loss
+        self.optimizer = optimizer
+        return modules
 
 
-def one_hot(elem, elems):
-    if isinstance(elems, int):
-        assert 0 <= elem < elems
-        elems = range(elems)
-    else:
-        assert len(set(elems)) == len(elems)
-    return [1 if e ==  elem else 0 for e in elems]
+    def parameters(self):
+        parameters = []
+        for m in self.modules:
+            parameters.extend(self.modules[m].parameters())
+        return parameters
 
-def variables_as_tensors(all_variables):
-    return {v: torch.tensor([one_hot(v, all_variables)], dtype=torch.float)
-                        for v in all_variables}
-
-############ TEST ###############################################
-term = 'b(Y,o(X,Z))'
-term_2 = 'k(X,t(t(b(o(X,Y),Z),o(Y,X)),U))'
-b_net = PredicteNetwork('b', 2)
-o_net = PredicteNetwork('b', 2)
-var_net = VariableNetwork()
-# TODO this should be read from file
-VARIABLES = ['X', 'Y', 'Z', 'U', 'W']
-PREDS_WITH_ARITIES = {
-    'e': 0,
-    'o': 2,
-    's': 3,
-    'b': 2,
-    'k': 2,
-    'a': 3,
-    't': 2,
-    'l': 3,
-    'r': 3
-}
-
-vs = variables_as_tensors(VARIABLES)
-#print(loss(torch.tensor([0.0], dtype=torch.float),
-#           torch.tensor([1.0], dtype=torch.float)))
-
-#print(one_hot(4, 10))
-#print(one_hot('a', ['a', 'b', 'c']))
-
-#tree_net = b_net(
-#    torch.cat(
-#        (var_net(vs['Y']), o_net(torch.cat((var_net(vs['X']),var_net(vs['Z'])),
-#                                           -1))),
-#        -1))
-#print(tree_net)
-
-parsed_term = parse(term)
-parsed_term_2 = parse(term_2)
-print(parsed_term_2)
-
-preds_with_arities = PREDS_WITH_ARITIES
-components = {} # Dictionary for instances of PredicteNetwork and ConstantNetwork
-for pred in preds_with_arities:
-    components[pred] = \
-        PredicteNetwork(pred, preds_with_arities[pred]) \
-                if preds_with_arities[pred] else \
-        ConstantNetwork(pred, preds_with_arities[pred])
-components['VAR'] = VariableNetwork()
-
-def tree(term):
-    if len(term) > 1:
-        x = torch.cat([tree(arg) for arg in term[1]], -1)
-        return components[term[0]](x)
-    else:
-        if term[0] in VARIABLES:
-            return components['VAR'](vs[term[0]])
-        else: # case of constant
-            assert not preds_with_arities[term[0]]
-            return components[term[0]](torch.tensor())
-
-#print(tree(parsed_term))
-#print(tree(parsed_term_2))
-eq = EqualityNetwork()
-#print(eq(torch.cat((tree(parsed_term), tree(parsed_term_2)), -1)))
-l = loss(1, eq(torch.cat((tree(parsed_term), tree(parsed_term_2)), -1)))
-print(l)
-
-
-sys.exit()
-############ TEST ENDED #########################################
-
-
-class TermNN(torch.nn.Module):
-    def __init__(self, preds_with_arities, variables):
-        """
-        Define and instantiate small component networks for each predicate.
-        """
-        super(TermNN, self).__init__()
-
-        self.components = {} # Dictionary for instances of PredicteNetwork
-        for pred in preds_with_arities:
-            self.components[pred] = PredicteNetwork(
-                pred,
-                preds_with_arities[pred]
-            )
 
     def tree(self, term):
-        if term[1]:
-            x = torch.cat([tree(c) for c in term[1]], -1)
-            return self.components[term[0]](x)
+        if len(term) > 1:
+            x = torch.cat([self.tree(t) for t in term[1]], -1)
+            return modules[term[0]](x)
         else:
-            return self.components['variable'](term[0])
-            # TODO case of constant e
-
-    def forward(self, term):
-        """
-        For given term compute forward pass in tree-like network which shape
-        corresponds to the shape of the term. Term is assumed to be of the parsed
-        form [predicate, list_of_arguments].
-        """
-        y_pred = tree(term)
-        return y_pred
+            return modules['VARCONST'](self.vars_consts_as_tensors[term[0]])
 
 
+    def forward(self, example):
+        term_L, term_R = example
+        return self.modules['EQUALITY'](torch.cat(
+            self.tree(term_L, self.modules, self.vars_consts_as_tensors),
+            self.tree(term_R, self.modules, self.vars_consts_as_tensors),
+            -1))
 
-#####################################
 
-DIM_IN_OUT, DIM_H = 64, 64
+    def train_one_example(self, example_with_label):
+        label, example = example_with_label
+        pred = self.forward(example)
+        loss = self.loss(pred, label)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        return loss.item(), pred.argmax().item()
 
 
-# TODO this should be read from file
-VARIABLES = ['X', 'Y', 'Z', 'U', 'W']
+    def train(self, examples, epochs=10, verbose=True): # examples with labels
+        for e in range(epochs):
+            losses = []
+            preds = []
+            for example in examples:
+                loss, pred = self.forward(example)
+                losses.append(loss)
+                preds.append(pred)
+            N = len(examples)
+            labels = [e[0].item() for e in examples]
+            loss_avg = sum(losses) / N
+            accuracy = sum(preds[i] == labels[i] for i in range(N)) / N
+            if verbose:
+                print("Loss on training {}. Accuracy on training {}.".format(
+                    loss_avg, accuracy))
 
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
 
-# TODO remove it
-test_example = 'k(X,t(t(b(o(X,Y),Z),o(Y,X)),U))'
+    def predict(inputs, model):
+        return [model(i).argmax().item() for i in inputs]
 
-# Construct our model by instantiating the class defined above
-model = DynamicNet(D_in, H, D_out)
 
-# Construct our loss function and an Optimizer. Training this strange model with
-# vanilla stochastic gradient descent is tough, so we use momentum
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
-for t in range(500):
-    # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x)
 
-    # Compute and print loss
-    loss = criterion(y_pred, y)
-    print(t, loss.item())
+############ TEST ###############################################
 
-    # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--train_set",
+    type=str,
+    help="Path to a training set.")
+parser.add_argument(
+    "--valid_set",
+    type=str,
+    help="Path to a validation set.")
+parser.add_argument(
+    "--test_set",
+    default='',
+    type=str,
+    help="Path to a testing set.")
+parser.add_argument(
+    "--model_path",
+    default='',
+    type=str,
+    help="Path where to save the trained model.")
+parser.add_argument(
+    "--epochs",
+    default=10,
+    type=int,
+    help="Number of epochs.")
+parser.add_argument(
+    "--embed_dim",
+    default=8,
+    type=int,
+    help="Token embedding dimension.")
+parser.add_argument(
+    "--threads",
+    default=1,
+    type=int,
+    help="Maximum number of threads to use.")
+parser.add_argument(
+    "--logdir",
+    default='',
+    type=str,
+    help="Logdir.")
+args = parser.parse_args()
+
+
+SYMBOLS_WITH_ARITIES = {
+    '+': 2,
+    '-': 2
+}
+
+labels_train, inputs_train = load_data(args.train_set)
+labels_valid, inputs_valid = load_data(args.valid_set)
+modulo = max(i.item() for i in labels_train + labels_valid) + 1
+numbers = set(''.join(inputs_train + inputs_valid)) - set(SYMBOLS_WITH_ARITIES)
+consts_as_tensors = consts_to_tensors(numbers)
+modules = instanciate_modules(SYMBOLS_WITH_ARITIES, len(numbers), modulo)
+params_of_modules = parameters_of_modules(modules)
+loss_1 = loss
+optim_1 = torch.optim.SGD(params_of_modules, lr=0.001, momentum=0.7)
+for e in range(args.epochs):
+    train(inputs_train, labels_train, modules, loss_1, optim_1)
+    acc = accuracy(inputs_valid, labels_valid, modules)
+    print("Epoch: {}. Accuracy on validation: {}".format(e, acc))
+
